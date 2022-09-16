722a81b6a98cb637fce4a9a8a771f65e14f61208
Saqlain <iamkhatik786@gmail.com>
2022-09-16 19:57:20 +0530

    Find max Float among three

--- GenericsTestMaximum.java
+++ GenericsTestMaximum.java
@@ -7,11 +7,36 @@ public class GenericsTestMaximum {
 		Integer fNum = 11;
 		Integer sNum = 21;
 		Integer tNum = 31;
+		Float fNumFloat = 11.1F;
+		Float sNumFloat = 21.1F;
+		Float tNumFloat = 31.1F;
 		
-		System.out.println("Maximum among three integer");
+		System.out.println("***** Test Maximum among three ***** ");
 		GenericsTestMaximum m = new GenericsTestMaximum();
 		m.findMax(fNum, sNum, tNum);
+		m.findMax(fNumFloat, sNumFloat, tNumFloat);
 		
+	}
+	
+public void findMax(Float fNumFloat, Float sNumFloat, Float tNumFloat) {
+	
+	Float maxNum;
+	
+	if(fNumFloat.compareTo(sNumFloat) > 0 && fNumFloat.compareTo(tNumFloat) > 0 )
+	{
+		maxNum = fNumFloat;
+	}
+	
+	else if(sNumFloat.compareTo(fNumFloat) > 0 && sNumFloat.compareTo(tNumFloat) > 0 )
+	{
+		maxNum = sNumFloat;
+	}
+	
+	else 
+		{
+			maxNum = tNumFloat;
+		}
+	System.out.println("Maximum float is : " + maxNum + " among " + fNumFloat + ", " + sNumFloat + " and " + tNumFloat);
 	}
 	
 public void findMax(Integer fNum, Integer sNum, Integer tNum) {
@@ -34,5 +59,4 @@ public void findMax(Integer fNum, Integer sNum, Integer tNum) {
 		}
 	System.out.println("Maximum integer is : " + maxNum + " among " + fNum + ", " + sNum + " and " + tNum);
 	}
-	
 }

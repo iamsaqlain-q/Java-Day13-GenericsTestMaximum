4a15d3496533b00fc008e4eb78822d3ddd38151f
Saqlain <iamkhatik786@gmail.com>
2022-09-16 19:45:13 +0530

    Find max integer among three

---
+++ MaximumInt.java
@@ -0,0 +1,38 @@
+package com.bl.generics;
+
+public class MaximumInt {
+
+	public static void main(String[] args) {
+	
+		Integer fNum = 11;
+		Integer sNum = 21;
+		Integer tNum = 31;
+		
+		System.out.println("Maximum among three integer");
+		MaximumInt m = new MaximumInt();
+		m.findMax(fNum, sNum, tNum);
+		
+	}
+	
+public void findMax(Integer fNum, Integer sNum, Integer tNum) {
+	
+	Integer maxNum;
+	
+	if(fNum.compareTo(sNum) > 0 && fNum.compareTo(tNum) > 0 )
+	{
+		maxNum = fNum;
+	}
+	
+	else if(sNum.compareTo(fNum) > 0 && sNum.compareTo(tNum) > 0 )
+	{
+		maxNum = sNum;
+	}
+	
+	else 
+		{
+			maxNum = tNum;
+		}
+	System.out.println("Maximum integer is : " + maxNum + " among " + fNum + ", " + sNum + " and " + tNum);
+	}
+	
+}
